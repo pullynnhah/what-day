6ac176a5bf13ae004cc3acf5a63bb8e763016f04
Paula Rodrigues <pullynnhah@icloud.com>
2023-07-19 14:42:05 -0300

    :tada: exercise 12 edit

--- index.ts
+++ index.ts
@@ -2,12 +2,14 @@ import express, { Request, Response } from "express";
 
 const app = express();
 
-app.get("/health", (req: Request, res: Response) => res.status(200).send("I'm Ok!"));
+app.get("/health", (req: Request, res: Response) =>
+  res.status(200).send("I'm Ok!")
+);
 app.get("/today", (req: Request, res: Response) => {
   res.send({
-    today: new Date().toLocaleDateString("pt-br")
-  })
+    today: new Date().toLocaleDateString("pt-br"),
+  });
 });
 
-const port: number = Number(process.env.PORT) || 5000;
+const port: number = Number(process.env.PORT) || 5001;
 app.listen(port, () => console.log(`Server is up and running on port ${port}`));
